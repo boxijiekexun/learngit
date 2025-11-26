d55c30022418fcc5cf20040ba9011ffa73bf21b0
202320944216 <202320944216@noreply.git.cmds.zhku.edu.cn>
2025-11-26 19:32:47 +0800

    hello world

--- example.cpp
+++ example.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main(){
   cout << "hello world" << endl;
+  cout << "This is git.  hello world" << endl;
+  cout << "hh" << endl;
   return 0;
+}
+void hello(){
+    cout << "oh yeah" << endl;
 }
